aa97e7c018a68487b20e41538884131bb844f62a
Gabriele Salvatori <salvatorigabriele@gmail.com>
2013-12-31 16:15:38 +0100

    Update sub.php

--- sub.php
+++ sub.php
@@ -1,31 +1,67 @@
 <?php
 
+if(isset($_POST['first_name'])) {
 
-$recipient = "enter the lists email address here";
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "you@yourdomain.com";
+    $email_subject = "Your email subject line";
 
-# SUBJECT (Subscribe/Remove)
-$subject = "Subscribe";
 
-# RESULT PAGE
-$location = "enter the URL of the result page here";
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
 
-## FORM VALUES ##
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
 
-# SENDER - WE ALSO USE THE RECIPIENT AS SENDER
-# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
-# SEE ALSO: How to protect a php Email Form using php mail or mb_send_mail against Mail Header Injection
-$sender = $recipient;
+    $first_name = $_POST['first_name']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
 
-# MAIL BODY
-$body .= "Name: ".$_REQUEST['Name']." \n";
-$body .= "Email: ".$_REQUEST['Email']." \n";
-# add more fields here if required
+    $error_message = "";
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+  }
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
 
-## SEND MESSGAE ##
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
 
-mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
 
-## SHOW RESULT PAGE ##
 
-header( "Location: $location" );
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+echo (int) mail($email_to, $email_subject, $email_message, $headers);  
+?>
+
+<!-- include your own success html here -->
+
+Thank you for contacting us. We will be in touch with you very soon.
+
+
+}
+
 ?>
